a34cac60d7b7e41755d5f9b2d720ded81f9f918c
Sabique A Khan <sabiquekhan@gmail.com>
2018-08-18 16:19:15 +0530

    Implementation for UNION FIND using Quick Union approach.
    1. Created the class QuickUnionUF for the implementation of UNION FIND problem using Quick Union (Lazy) approach.

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Algorithms.UNION_FIND;
+using System.Diagnostics;
 
 namespace Algorithms
 {
@@ -10,6 +12,51 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            int totalElement = 10;
+            QuickUnionUF qu = new QuickUnionUF(totalElement);
+            QuickFindUF qf = new QuickFindUF(totalElement);
+
+            #region Quick Union Test
+            Stopwatch swQU = new Stopwatch();
+            swQU.Start();
+            //perform the union operation to connect the components
+            qu.Union(6, 5);
+            qu.Union(2, 9);
+            qu.Union(4, 3);
+            qu.Union(9, 4);
+            qu.Union(3, 6);
+
+            //check if the components are connected or not
+            Console.WriteLine("0 & 1 are connected - " + qu.Connected(0, 1));
+            Console.WriteLine("1 & 5 are connected - " + qu.Connected(1, 5));
+            Console.WriteLine("1 & 2 are connected - " + qu.Connected(1, 2));
+            Console.WriteLine("3 & 6 are connected - " + qu.Connected(3, 6));
+            Console.WriteLine("2 & 3 are connected - " + qu.Connected(2, 3));
+            swQU.Stop();
+            #endregion
+
+            #region Quick Find Test 
+            Stopwatch swQF = new Stopwatch();
+            swQF.Start();
+            //perform the union operation to connect the components
+            qf.Union(6, 5);
+            qf.Union(2, 9);
+            qf.Union(4, 3);
+            qf.Union(9, 4);
+            qf.Union(3, 6);
+
+            //check if the components are connected or not
+            Console.WriteLine("0 & 1 are connected - " + qf.Connected(0, 1));
+            Console.WriteLine("1 & 5 are connected - " + qf.Connected(1, 5));
+            Console.WriteLine("1 & 2 are connected - " + qf.Connected(1, 2));
+            Console.WriteLine("3 & 6 are connected - " + qf.Connected(3, 6));
+            Console.WriteLine("2 & 3 are connected - " + qf.Connected(2, 3));
+            swQF.Stop();
+            #endregion
+
+            Console.WriteLine("Total time taken to perform operations using Quick Union: " + swQU.Elapsed);
+            Console.WriteLine("Total time taken to perform operations using Quick Find: " + swQF.Elapsed);
+            Console.ReadLine();
         }
     }
 }
---
+++ Algorithms/Algorithms/UNION-FIND/QuickUnionUF.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.UNION_FIND
+{
+    class QuickUnionUF
+    {
+        private int[] id;
+
+        //constructor to initialize the array for the UNION FIND
+        public QuickUnionUF(int n)
+        {
+            id = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+        }
+
+        //method to find the root of the element
+        private int root(int i)
+        {
+            while (i != id[i]) i = id[i];
+            return i;
+        }
+
+        //method to check if the two componenets are connected
+        public bool Connected(int p, int q)
+        {
+            return root(p) == root(q);
+        }
+
+        //method to connect the two componenets
+        public void Union(int p, int q)
+        {
+            int i = root(p);
+            int j = root(q);
+            id[i] = j;
+        }
+
+        public static void Main()
+        {
+            //initialize the components
+            QuickUnionUF uf = new QuickUnionUF(10);
+
+            //perform the union operation to connect the components
+            uf.Union(1, 4);
+            uf.Union(4, 5);
+            uf.Union(2, 3);
+            uf.Union(2, 6);
+            uf.Union(3, 6);
+            uf.Union(3, 7);
+
+            //check the elements with their component group in the array
+            for (int i = 0; i < uf.id.Length; i++)
+            {
+                Console.WriteLine(i + " - " + uf.id[i]);
+            }
+
+            //check if the components are connected or not
+            Console.WriteLine("0 & 1 are connected - " + uf.Connected(0, 1));
+            Console.WriteLine("1 & 5 are connected - " + uf.Connected(1, 5));
+            Console.WriteLine("1 & 2 are connected - " + uf.Connected(1, 2));
+            Console.WriteLine("3 & 6 are connected - " + uf.Connected(3, 6));
+            Console.WriteLine("2 & 3 are connected - " + uf.Connected(2, 3));
+            Console.ReadLine();
+        }
+    }
+}
